d783f4b5862aedf8bfd95634617501290ad24ad8
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-01-09 06:58:17 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : EXCEL-MDX
+ * @author : Xcelsama <https://github.com/xcelsama>
+ * @description : Excel-mdx,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
@@ -21,7 +21,7 @@ Secktor.cmd({
         var inital = new Date().getTime();
         const { key } = await Void.sendMessage(citel.chat, {text: '```Excel is Pinging!!!```'});
         var final = new Date().getTime();
-       // await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: '*Pong🔰*\n *' + (final - inital) + ' ms* ', edit: key});
+       // await Excel-mdx.sleep(1000)
+       return await Void.sendMessage(citel.chat, {text: 'Excel-mdx ⚡'*Pong🔰*\n *' + (final - inital) + ' ms* ', edit: key});
     }
 );

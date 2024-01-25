e8dc1925e409c17e0097e1b910126e2864dbc78f
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-01-25 10:38:01 +0000

    Update pokemon.js

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,32 +1,27 @@
-import fetch from 'node-fetch'; 
-  
- let handler = async (m, { conn, text }) => { 
-   if (!text) throw 'Please provide a Pokemon name to search for.'; 
-  
-   const url = `https://some-random-api.com/pokemon/pokedex?pokemon=${encodeURIComponent(text)}`; 
-  
-   const response = await fetch(url); 
-   const json = await response.json(); 
-  
-   if (!response.ok) { 
-     throw `An error occurred: ${json.error}`; 
-   } 
-  
-   const message = ` 
- *Name:* ${json.name} 
- *ID:* ${json.id} 
- *Type:* ${json.type} 
- *Abilities:* ${json.abilities} 
- *Height:* ${json.height} 
- *Weight:* ${json.weight} 
- *Description:* ${json.description} 
- `; 
-  
-   conn.sendMessage(m.chat, { text: message }, 'extendedTextMessage', { quoted: m }); 
- }; 
-  
- handler.help = ['pokedex <pokemon>']; 
- handler.tags = ['anime']; 
- handler.command = /^pokedex/i; 
-  
- export default handler;
+js
+module.exports = async (citel, text) => {
+  const { commands } = require('../lib');
+
+  let arr = [];
+  const cmd = commands.find((cmd) => cmd.pattern === (text.split(" ")[0].toLowerCase()));
+  if (!cmd) return await citel.reply("*❌ No Such commands.*");
+
+  arr.push(`*🍁 Please provide a Pokémon name to search for.'; 
+  :* ${cmd.pattern}`);
+  const url = `https://some-random-api.com/pokemon/pokedex?pokemon=${encodeURIComponent(text)}`;
+  const response = await fetch(url);
+  const json = await response.json();
+  if (!response.ok) {
+    throw `An error occurred: ${json.error}`;
+  }
+  const message = `
+*Name:* ${json.name}
+*ID:* ${json.id}
+*Type:* ${json.type}
+*Abilities:* ${json.abilities}
+*Height:* ${json.height}
+*Weight:* ${json.weight}
+*Description:* ${json.description}
+`;
+  citel.reply(message);
+};
